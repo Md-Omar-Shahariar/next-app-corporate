343fd9be22dca8045571f3b18d921008487bc9c6
omar afridi <osafridi00@gmail.com>
2024-01-03 17:57:43 +0600

    link

---
+++ src/app/performance/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Performance = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Performance</h1>
+    </div>
+  );
+};
+
+export default Performance;
