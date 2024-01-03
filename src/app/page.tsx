494cc022218f5981bcd32e0b1102e26bdb497f08
omar afridi <osafridi00@gmail.com>
2024-01-03 19:45:40 +0600

    link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import React from "react";
 export default function LandingPage() {
   return (
     <div>
-      <Link href={"/performance"}>Performance</Link>
       <h1 className="h-dvh text-3xl ">LandingPage</h1>
     </div>
   );
---
+++ src/app/scale/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Scale = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Scale</h1>
+    </div>
+  );
+};
+
+export default Scale;
